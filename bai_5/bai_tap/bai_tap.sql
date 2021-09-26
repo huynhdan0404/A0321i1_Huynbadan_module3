9cf58d1c33e5830a38142daf4836869a90e308e7
A0321I1-HuynhBaDan <huynhbadank16@gmail.com>
2021-09-26 15:44:58 +0700

    nopbaitapbai

--- bai_5/bai_tap/bai_tap.sql
+++ bai_5/bai_tap/bai_tap.sql
@@ -15,3 +15,43 @@ explain select * from product where product_code = "b";
 
 alter table product add index name_price(product_name,product_price);
 explain select * from product where product_name = "cafe" or product_price = 500;
+
+create view product_view as
+select product.product_code, product.product_name, product.product_price, product.product_status 
+from product;
+
+create or replace view product_view as
+select  product.product_code, product.product_name, product.product_price
+from product;
+
+drop view product_view;
+
+DELIMITER //
+create procedure product_funtion()
+begin
+select * from product;
+end //
+DELIMITER ;
+
+call product_funtion();
+
+DELIMITER //
+create procedure product_add()
+begin
+insert into product (id,product_code,product_name,product_price)
+value (4,"d","trứng",400);
+end //
+DELIMITER ;
+drop procedure product_add;
+call product_add();
+
+
+DELIMITER //
+create procedure product_delete(a int)
+begin
+delete from product where product.id = a;
+end //
+DELIMITER ;
+call product_delete(4);
+
+
